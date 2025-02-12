971dad00eadbd1c8167f2976619cd598065c319a
darkustarku <ahmed.abdelelamin@gmail.com>
2025-02-12 00:43:12 -0500

    forgot to commit this too, unacceptable

--- books/scripts/script.js
+++ books/scripts/script.js
@@ -5,6 +5,15 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+
+    this.changeStatus = function () {
+      if (this.read == true) {
+        this.read = false;
+      }
+      else {
+        this.read = true;
+      }
+    }
   
   
     this.info = function () {
@@ -12,16 +21,23 @@ function Book(title, author, pages, read) {
     }
   }
 
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+  );
+}
 
 function addBookToLibrary (title, author, pages, read) {
-    book = new Book(title, author, pages, read);
+    const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
 
 function displayLibrary (Library) {
-    for (book in Library) {
-        const container = document.querySelector("container");
+
+    for (let i =0; i < Library.length; i++) {
+        let book = Library[i];
 
         const div = document.createElement("div");
         div.classList.add("book");
@@ -41,15 +57,119 @@ function displayLibrary (Library) {
         pages.classList.add("pages");
         div.appendChild(pages);
 
-        const read = document.createElement("p");
+        const read = document.createElement("span");
         read.innerHTML = book.read;
-        read.classList.add("read");
+        read.classList.add("status");
         div.appendChild(read);
 
 
-        document.getElementById("container").appendChild(div);
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete Book";
+        deleteButton.classList.add("delete-button");
+        div.appendChild(deleteButton);
+
+        div.dataset.index = i;
+
+        const statusButton = document.createElement("button");
+        statusButton.innerHTML = "Change Status";
+        statusButton.classList.add("status-button");
+        div.appendChild(statusButton);
+
+        document.getElementsByClassName("container")[0].appendChild(div)
+
+        deleteButton.addEventListener("click", (event) => {
+          removeBook(event);
+        });
+
+        statusButton.addEventListener("click", (event) => {
+          const div = event.target.parentNode;
+          index = div.dataset.index;
+          myLibrary[index].changeStatus();
+          const status = myLibrary[index].read;
+          const read = div.querySelector(".status");
+          read.innerHTML = status;
+        });
+
     }
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 304, true);
-displayLibrary(myLibrary);
+function removeBook(event) {
+  const div = event.target.parentNode;
+  const container = div.parentNode;
+  const index = div.dataset.index;
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (i == index) {
+      myLibrary.splice(index, 1);
+      console.log(myLibrary);
+  }
+  div.remove();
+  updateIDs(container);
+}
+}
+
+function updateIDs (container) {
+  if (container.children != null) {
+    console.log(container.children)
+    const books = container.children;
+    for (let i = 0; i < books.length; i++) {
+      const book = books[i];
+      book.dataset.index = i;
+    }
+  }
+}
+
+
+const showButton = document.querySelector("#showdialog");
+const dialog = document.querySelector("dialog");
+
+
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+const closeButton = document.querySelector("#close");
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+
+dialog.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const bookInfo = document.getElementById("bookform").elements;
+  const bookName = bookInfo[0].value;
+  const bookAuthor = bookInfo[1].value;
+  const bookPages = bookInfo[2].value;
+  let bookStatus;
+
+  if (bookInfo[4] != undefined) {
+    bookStatus = "read";
+  }
+  else {
+    bookStatus = "not read";
+  }
+
+  addBookToLibrary(bookName, bookAuthor, bookPages, bookStatus);
+
+  clearContainer();
+  displayLibrary(myLibrary);
+
+  dialog.close();
+  resetForm();
+});
+
+function resetForm() {
+  const form = document.getElementById("bookform");
+  form.reset();
+}
+
+function clearContainer () {
+  const container = document.getElementById("book-container");
+  container.innerHTML = "";
+}
+
+
+
+
+
